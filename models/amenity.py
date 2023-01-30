ebb5b64ca3621d58667dc3508757a770314b3ee6
Mohamed Jarraya <mohamedjarraya@Mohameds-MacBook-Air.local>
2023-01-30 11:29:32 +0100

    new

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""This is the amenity class @author: @medjarraya"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
